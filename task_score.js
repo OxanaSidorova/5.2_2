98c91eab66a324926957573940fb8e737d6e6aef
OxanaSidorova <106552165+OxanaSidorova@users.noreply.github.com>
2023-01-23 13:55:33 +0000

    Task 5.4

--- task_score.js
+++ task_score.js
@@ -1,9 +1,15 @@
 let intriduction = 50;
 let git = 80;
 let js = 90;
-let average_score = (intriduction+git+js)/3;
+let average_score = ((intriduction+git+js)/3).toFixed(2);
+
 console.log (`This is average student score - ${average_score}.`);
-let studentName = "Oxana Sidorova";
-console.log (`${studentName}'s average score is ${average_score}.`)
+let studentName = "Oxana ";
+let studentSurname = "Sidorova"
+let studentFullName = studentName + studentSurname
+
+console.log (`${studentFullName} average score is ${average_score}.`)
 
 
+let short_name = studentName.slice(0,1)
+console.log(`${short_name}. ${studentSurname}`)
